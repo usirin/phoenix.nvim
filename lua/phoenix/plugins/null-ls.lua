a2b73335ee583ec2fe81664ae02e27f8e26cdf17
usirin <umutsirin1@gmail.com>
2023-04-29 02:55:08 -0700

    feat: add null-ls

---
+++ lua/phoenix/plugins/null-ls.lua
@@ -0,0 +1,37 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    local b = null_ls.builtins
+
+    local sources = {
+      -- formatting
+      b.formatting.prettierd,
+      b.formatting.stylua.with({
+        condition = function(utils)
+          return utils.root_has_file({ "stylua.toml", ".stylua.toml" })
+        end,
+      }),
+
+      b.formatting.goimports.with({
+        args = {
+          "-srcdir", "$DIRNAME", "-local=code.justin.tv", "-w", "cmd", "errorutils", "internal", "loaders", "resolvers"
+        }
+      }),
+
+      b.formatting.beautysh,
+
+      -- require("typescript.extensions.null-ls.code-actions"),
+
+      -- b.formatting.eslint_d,
+
+      -- b.diagnostics.eslint_d,
+      -- b.code_actions.eslint_d,
+    }
+
+    null_ls.setup({
+      -- debug = true,
+      sources = sources,
+    })
+  end
+}
