aa92085f5121b4264f04f8888a0dbb2d7dcf5fba
usirin <umutsirin1@gmail.com>
2023-05-01 17:30:31 -0700

    feat: update default lsp servers

--- init.lua
+++ init.lua
@@ -490,11 +490,11 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
+  gopls = {},
+  graphql = {},
   tsserver = {},
+  prismals = {},
+  jsonls = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
