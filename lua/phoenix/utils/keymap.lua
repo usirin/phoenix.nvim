d91a8cb2b552fcd9a9f3b9ee0c3c84290da383e1
usirin <umutsirin1@gmail.com>
2023-10-14 01:25:06 -0700

    keymap: use vim.keymap.set

--- lua/phoenix/utils/keymap.lua
+++ lua/phoenix/utils/keymap.lua
@@ -1,5 +1,5 @@
 ---@param mode string    Mode short-name, see |nvim_set_keymap()|. Can also be list of modes to create mapping on multiple modes.
----@param lhs string           Left-hand side |{lhs}| of the mapping.
+---@param lhs string|table           Left-hand side |{lhs}| of the mapping.
 ---@param rhs string|function  Right-hand side |{rhs}| of the mapping, can be a Lua function.
 ---@param opts table|nil Table of |:map-arguments|. Defaults to `{noremap = true, silent = true}`.
 ---@see |nvim_set_keymap()|
@@ -8,7 +8,7 @@ local keymap = function(mode, key, action, opts)
   if opts then
     options = vim.tbl_extend('force', options, opts)
   end
-  vim.api.nvim_set_keymap(mode, key, action, options)
+  vim.keymap.set(mode, key, action, options)
 end
 
 return keymap
