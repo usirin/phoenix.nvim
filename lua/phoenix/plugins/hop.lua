049791f02262e2ef38772498979ee89fb366f01a
Umut Sirin <umut.sirin@discordapp.com>
2025-08-29 17:23:55 -0700

    [hop] fix initialization

--- lua/phoenix/plugins/hop.lua
+++ lua/phoenix/plugins/hop.lua
@@ -2,10 +2,11 @@ local map = require 'phoenix.utils.keymap'
 
 return {
   'smoka7/hop.nvim',
-  opts = {
-    keys = 'arstgmneiodh',
-  },
+  version = '*',
   config = function()
+    require('hop').setup {
+      keys = 'arstgmneiodh',
+    }
     map('n', '<C-e>', "<cmd>lua require'hop'.hint_words()<cr>", { noremap = true, silent = true })
   end,
 }
